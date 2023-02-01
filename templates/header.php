aaafdb2ed065544ad78712068b8f433089f5d9c4
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-02-01 19:25:56 +0100

    initial commit

---
+++ templates/header.php
@@ -0,0 +1,38 @@
+<head>
+	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	 <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  <title>Ninja Pizza</title>
+</head>
+<style>
+	.brand-text{
+		color: #cbb09c !important;
+	}
+	.brand-bg{
+		background-color: #cbb09c !important;
+	}
+</style>
+<body style="font-family: Poppins; background-color: #f4f4f4">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-lg">
+    <a class="navbar-brand fw-bolder fs-3 brand-text" href="#">Ninja Pizza</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="btn text-white brand-bg"href="#">Add a pizza</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
